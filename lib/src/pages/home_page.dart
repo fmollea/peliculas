a12701d74d4ab512df2ccb5895d7787340bb6e6d
Federico <federico.mollea@mercadolibre.com>
2019-10-03 01:06:15 -0300

    se crea un file widget card swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,20 +31,6 @@ class HomePage extends StatelessWidget {
 
   Widget _swiperTarjetas() {
 
-    return Container(
-      width: double.infinity,
-      height: 150,
-      margin: EdgeInsets.only(top: 16),
-      child: new Swiper(
-        itemBuilder: (BuildContext context,int index){
-          return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-        },
-        itemCount: 3,
-        pagination: new SwiperPagination(),
-        itemWidth: 350,
-        itemHeight: 150,
-        layout: SwiperLayout.STACK,
-      ),
-    );
+    return CardSwiper(peliculas: [1,2,3],);
   }
 }
