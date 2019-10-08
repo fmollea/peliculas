fab58128b72d8a5ebbceea15320de0eb4994620a
Federico <federico.mollea@mercadolibre.com>
2019-10-08 20:51:02 -0300

    get peliculas

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
 
 class CardSwiper extends StatelessWidget {
 
-  final List<dynamic> peliculas;
+  final List<Pelicula> peliculas;
 
   CardSwiper({
     @required this.peliculas
@@ -18,10 +19,15 @@ class CardSwiper extends StatelessWidget {
       margin: EdgeInsets.only(top: 16),
       child: new Swiper(
         
-        itemBuilder: (BuildContext context,int index){
+        itemBuilder: (BuildContext context, int index){
           return ClipRRect(
             borderRadius: BorderRadius.circular(12.0),
-            child: new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.cover,));
+            child: FadeInImage(
+              image: NetworkImage(peliculas[index].getPosterImg()),
+              placeholder: AssetImage('assets/loading.gif'),
+              fit: BoxFit.cover,
+            )
+          );
         },
         itemWidth: _screenSize.width * 0.7,
         itemHeight: _screenSize.height * 0.5,
