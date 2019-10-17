b0b9fa6f4a90d82dbea15ce99ce3dcb71539e0fa
Federico <fmollea@gmail.com>
2019-10-17 00:08:51 -0300

    implementación de streams y optimizaciones (#6)
    videos 103 / 111

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
 
@@ -8,6 +9,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    provider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Peliculas'),
@@ -22,8 +25,10 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             _swiperTarjetas(),
+            _footer(context),
           ],
         ),
       ),
@@ -31,8 +36,6 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-
-    
 
     return FutureBuilder(
       future: provider.getEnCines(),
@@ -48,8 +51,40 @@ class HomePage extends StatelessWidget {
             child: Center(
               child: CircularProgressIndicator()));
         }
-        
       },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(left: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Populares', style: Theme.of(context).textTheme.subhead),
+          SizedBox(height: 8.0),
+          StreamBuilder(
+              stream: provider.popularesStream,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+
+                Widget resultView;
+
+                if (snapshot.hasData) {
+                  resultView = MovieHorizontal(
+                    peliculas: snapshot.data,
+                    siguientePagina: provider.getPopulares
+                    );
+                } else {
+                  resultView = CircularProgressIndicator();
+                }
+
+                return resultView;
+              },
+            ),
+        ],
+      ),
     );
   }
 }
